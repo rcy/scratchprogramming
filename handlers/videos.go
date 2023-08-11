774185a357e6d9228fc4c78d834e68363921ace6
Ryan Yeske <ryan@ryanyeske.com>
2023-08-10 18:29:44 -0700

    Add Channels

--- handlers/videos.go
+++ handlers/videos.go
@@ -12,39 +12,60 @@ import (
 func Videos(w http.ResponseWriter, r *http.Request) {
 	parts := strings.Split(r.URL.Path, "/")
 
-	log.Printf("VideosIndex %s request %s", r.URL, parts[2])
+	log.Printf("VideosIndex %s request %s", r.URL, parts)
 
-	if parts[2] != "" {
-		renderPlayer(w, parts[2])
+	if len(parts) == 4 {
+		renderPlayer(w, parts[2], parts[3])
 	} else {
-		renderIndex(w)
+		renderIndex(w, parts[2])
 	}
 }
 
-func renderPlayer(w http.ResponseWriter, filename string) {
+func renderPlayer(w http.ResponseWriter, subdir string, filename string) {
 	vf := VideoFile{
-		Name: filename,
+		SubDir: subdir,
+		Name:   filename,
 	}
 
-	err := PlayerTemplate.Execute(w, vf)
+	subdirs, err := getSubDirs()
+	if err != nil {
+		w.WriteHeader(500)
+	}
+
+	err = PlayerTemplate.Execute(w, struct {
+		SubDirs   []string
+		VideoFile VideoFile
+	}{
+		SubDirs:   subdirs,
+		VideoFile: vf,
+	})
 
 	if err != nil {
 		log.Printf("error rendering player: %s", err)
 	}
 }
 
-func renderIndex(w http.ResponseWriter) {
+func renderIndex(w http.ResponseWriter, subdir string) {
 	// collect list of video filenames from assets/videos
 
-	vfs, err := getVideos()
+	vfs, err := getVideos(subdir)
+	if err != nil {
+		w.WriteHeader(500)
+	}
+
+	subdirs, err := getSubDirs()
 	if err != nil {
 		w.WriteHeader(500)
 	}
 
 	err = VideosTemplate.Execute(w, struct {
+		SubDir     string
 		VideoFiles []VideoFile
+		SubDirs    []string
 	}{
+		SubDir:     subdir,
 		VideoFiles: vfs,
+		SubDirs:    subdirs,
 	})
 
 	if err != nil {
@@ -53,12 +74,15 @@ func renderIndex(w http.ResponseWriter) {
 }
 
 type VideoFile struct {
+	SubDir  string
 	Name    string
 	ModTime time.Time
 }
 
-func getVideos() ([]VideoFile, error) {
-	dir := os.Getenv("ASSET_DIRECTORY") + "/videos"
+func getVideos(subdir string) ([]VideoFile, error) {
+	dir := os.Getenv("ASSET_DIRECTORY") + "/videos/" + subdir
+
+	log.Printf("getVideos dir=%s", dir)
 
 	vfs := []VideoFile{}
 
@@ -82,4 +106,27 @@ func getVideos() ([]VideoFile, error) {
 	})
 
 	return vfs, nil
+}
+
+func getSubDirs() ([]string, error) {
+	dir := os.Getenv("ASSET_DIRECTORY") + "/videos/"
+
+	subdirs := []string{}
+
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		return []string{}, err
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			subdirs = append(subdirs, file.Name())
+		}
+	}
+
+	sort.Slice(subdirs, func(i int, j int) bool {
+		return subdirs[i] > subdirs[j]
+	})
+
+	return subdirs, nil
 }
